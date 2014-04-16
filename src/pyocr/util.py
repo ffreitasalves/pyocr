f23158fcdde284adc6a1c3ed0de509c2776e2a55
Fernando Freitas Alves <ffreitasalves@gmail.com>
2014-04-16 13:25:42 -0300

    finding tesseract on windows

--- src/pyocr/util.py
+++ src/pyocr/util.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 
 def to_unicode(string):
@@ -17,8 +18,20 @@ def is_on_path(exec_name):
         True --- if it is installed
         False --- if it isn't
     """
-    for dirpath in os.environ["PATH"].split(os.pathsep):
-        path = os.path.join(dirpath, exec_name)
-        if os.path.exists(path) and os.access(path, os.X_OK):
-            return True
+    if sys.platform == 'win32':
+        #Tesseract keeps its own Environment Variable on Windows
+        all_paths = os.environ["PATH"].split(os.pathsep)
+        if os.environ.get('TESSDATA_PREFIX'):
+            all_paths.append(os.environ.get('TESSDATA_PREFIX'))
+
+        for dirpath in all_paths:
+            path = os.path.join(dirpath, exec_name + '.exe')
+            if os.path.exists(path) and os.access(path, os.X_OK):
+                return True
+    else:
+        for dirpath in os.environ["PATH"].split(os.pathsep):
+            path = os.path.join(dirpath, exec_name)
+            if os.path.exists(path) and os.access(path, os.X_OK):
+                return True
+
     return False
